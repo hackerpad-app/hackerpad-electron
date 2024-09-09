06dd7b24332dfab2f3cd862b89251baa9aa7495b
Miko <mikolajkacki98@gmail.com>
2024-09-09 21:11:13 +0100

    Add lupa 🇵🇱.

--- src/renderer/src/components/common/EditorTools.tsx
+++ src/renderer/src/components/common/EditorTools.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { PiNotePencilLight } from 'react-icons/pi'
 import { AiOutlineDelete } from 'react-icons/ai'
 import { useNotesContext } from './../context/NotesContext'
+import { FaSearch } from 'react-icons/fa'
 
 interface ToolsProps {
   pad: string
@@ -65,13 +66,14 @@ const Tools = ({ pad }: ToolsProps) => {
             </div>
           </button>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center relative">
           <input
             type="text"
             placeholder="Search..."
             onChange={handleSearchChange}
-            className="bg-transparent rounded-md text-gray-300 px-3 py-1 mr-2 ring-gray-300 ring-1 focus:outline-none w-64"
+            className="bg-transparent rounded-md text-gray-300 pl-8 pr-3 py-1 mr-2 ring-gray-300 ring-1 focus:outline-none w-64"
           />
+          <FaSearch className="absolute left-2 text-gray-300" />
         </div>
       </div>
     </div>
